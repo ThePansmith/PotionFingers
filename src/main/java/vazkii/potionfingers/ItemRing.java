17e15f96433f74668dad6f62ae6c13f9976fca86
Pyrofab <redstoneinfire@gmail.com>
2019-11-05 22:02:07 +0100

    Reformat code

--- src/main/java/vazkii/potionfingers/ItemRing.java
+++ src/main/java/vazkii/potionfingers/ItemRing.java
@@ -24,111 +24,111 @@ import vazkii.arl.util.ItemNBTHelper;
 
 public class ItemRing extends ItemMod implements IBauble, IItemColorProvider {
 
-	private static final String TAG_POTION_EFFECT = "effect";
+    private static final String TAG_POTION_EFFECT = "effect";
 
-	private static final String[] VARIANTS = new String[] {
-		"ring_disabled",
-		"ring_enabled"
-	};
-	public static final int EFFECT_DURATION = 199;
-	public static final int EFFECT_REFRESH_RATE = EFFECT_DURATION / 2;
+    private static final String[] VARIANTS = new String[]{
+            "ring_disabled",
+            "ring_enabled"
+    };
+    public static final int EFFECT_DURATION = 199;
+    public static final int EFFECT_REFRESH_RATE = EFFECT_DURATION / 2;
 
-	public ItemRing() {
-		super("ring", VARIANTS);
-		setCreativeTab(CreativeTabs.BREWING);
-		setMaxStackSize(1);
-		setHasSubtypes(true);
-	}
+    public ItemRing() {
+        super("ring", VARIANTS);
+        setCreativeTab(CreativeTabs.BREWING);
+        setMaxStackSize(1);
+        setHasSubtypes(true);
+    }
 
-	@Override
-	public String getModNamespace() {
-		return PotionFingers.MOD_ID;
-	}
+    @Override
+    public String getModNamespace() {
+        return PotionFingers.MOD_ID;
+    }
 
-	@Override
-	public String getUniqueModel() {
-		return "ring";
-	}
+    @Override
+    public String getUniqueModel() {
+        return "ring";
+    }
 
-	@Override
-	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> subItems) {
-		if(tab == getCreativeTab()) {
-			subItems.add(new ItemStack(this));
-			for(Potion p : PotionFingers.DEFAULT_EFFECTS)
-				subItems.add(getRingForPotion(p));
-		}
-	}
+    @Override
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> subItems) {
+        if (tab == getCreativeTab()) {
+            subItems.add(new ItemStack(this));
+            for (Potion p : PotionFingers.DEFAULT_EFFECTS)
+                subItems.add(getRingForPotion(p));
+        }
+    }
 
-	@Override
-	public boolean hasEffect(ItemStack stack) {
-		return getPotion(stack) != null;
-	}
+    @Override
+    public boolean hasEffect(ItemStack stack) {
+        return getPotion(stack) != null;
+    }
 
-	@Override
-	public String getItemStackDisplayName(ItemStack stack) {
-		String name = super.getItemStackDisplayName(stack);
-		Potion p = getPotion(stack);
-		String potionName = "N/A";
-		if(p != null)
-			potionName = I18n.translateToLocal(p.getName());
+    @Override
+    public String getItemStackDisplayName(ItemStack stack) {
+        String name = super.getItemStackDisplayName(stack);
+        Potion p = getPotion(stack);
+        String potionName = "N/A";
+        if (p != null)
+            potionName = I18n.translateToLocal(p.getName());
 
-		return String.format(name, potionName);
-	}
+        return String.format(name, potionName);
+    }
 
-	public static ItemStack getRingForPotion(Potion potion) {
-		String id = potion.getRegistryName().toString();
-		ItemStack stack = new ItemStack(PotionFingers.ring, 1, 1);
-		ItemNBTHelper.setString(stack, TAG_POTION_EFFECT, id);
-		return stack;
-	}
+    public static ItemStack getRingForPotion(Potion potion) {
+        String id = potion.getRegistryName().toString();
+        ItemStack stack = new ItemStack(PotionFingers.ring, 1, 1);
+        ItemNBTHelper.setString(stack, TAG_POTION_EFFECT, id);
+        return stack;
+    }
 
-	public static Potion getPotion(ItemStack stack) {
-		if(stack == null)
-			return null;
+    public static Potion getPotion(ItemStack stack) {
+        if (stack == null)
+            return null;
 
-		String effect = ItemNBTHelper.getString(stack, TAG_POTION_EFFECT, "");
-		if(effect.isEmpty()) {
+        String effect = ItemNBTHelper.getString(stack, TAG_POTION_EFFECT, "");
+        if (effect.isEmpty()) {
             return null;
         }
 
-		return Potion.REGISTRY.getObject(new ResourceLocation(effect));
-	}
+        return Potion.REGISTRY.getObject(new ResourceLocation(effect));
+    }
 
-	@Override
-	@SideOnly(Side.CLIENT)
-	public IItemColor getItemColor() {
-		return new IItemColor() {
-			@Override
-			public int colorMultiplier(ItemStack stack, int i) {
-				if(i != 0) {
-					Potion p = getPotion(stack);
-					if(p != null)
-						return p.getLiquidColor();
-				}
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IItemColor getItemColor() {
+        return new IItemColor() {
+            @Override
+            public int colorMultiplier(ItemStack stack, int i) {
+                if (i != 0) {
+                    Potion p = getPotion(stack);
+                    if (p != null)
+                        return p.getLiquidColor();
+                }
 
-				return 0xFFFFFF;
-			}
-		};
-	}
+                return 0xFFFFFF;
+            }
+        };
+    }
 
-	@Override
-	public void onEquipped(ItemStack itemstack, EntityLivingBase player) {
-		updatePotionStatus(player, itemstack, false);
-	}
+    @Override
+    public void onEquipped(ItemStack itemstack, EntityLivingBase player) {
+        updatePotionStatus(player, itemstack, false);
+    }
 
-	@Override
-	public void onUnequipped(ItemStack itemstack, EntityLivingBase player) {
-		updatePotionStatus(player, itemstack, true);
-	}
+    @Override
+    public void onUnequipped(ItemStack itemstack, EntityLivingBase player) {
+        updatePotionStatus(player, itemstack, true);
+    }
 
     @SubscribeEvent
     public void onTickPlayerTick(TickEvent.PlayerTickEvent event) {
-	    if (event.phase == TickEvent.Phase.START) return;
-	    if (event.side == Side.CLIENT) return;
-	    if (event.player.ticksExisted % EFFECT_REFRESH_RATE != 0) return;
+        if (event.phase == TickEvent.Phase.START) return;
+        if (event.side == Side.CLIENT) return;
+        if (event.player.ticksExisted % EFFECT_REFRESH_RATE != 0) return;
 
-		EntityPlayer player = event.player;
-		IBaublesItemHandler inv = BaublesApi.getBaublesHandler(player);
+        EntityPlayer player = event.player;
+        IBaublesItemHandler inv = BaublesApi.getBaublesHandler(player);
 
         for (int slot : BaubleType.RING.getValidSlots()) {
             ItemStack ring1 = inv.getStackInSlot(slot);
@@ -142,47 +142,47 @@ public class ItemRing extends ItemMod implements IBauble, IItemColorProvider {
                         break;
                     }
                 }
-				addPotionEffect(player, level, potion);
-			}
+                addPotionEffect(player, level, potion);
+            }
         }
     }
 
-	private void addPotionEffect(EntityLivingBase player, int level, Potion potion) {
-		player.addPotionEffect(new PotionEffect(potion, EFFECT_DURATION, level, true, false));
-	}
+    private void addPotionEffect(EntityLivingBase player, int level, Potion potion) {
+        player.addPotionEffect(new PotionEffect(potion, EFFECT_DURATION, level, true, false));
+    }
 
-	public void updatePotionStatus(EntityLivingBase player, ItemStack ring, boolean unequipping) {
-	    Potion potion = getPotion(ring);
-		if(potion == null || !(player instanceof EntityPlayer))
-			return;
+    public void updatePotionStatus(EntityLivingBase player, ItemStack ring, boolean unequipping) {
+        Potion potion = getPotion(ring);
+        if (potion == null || !(player instanceof EntityPlayer))
+            return;
 
-		IBaublesItemHandler inv = BaublesApi.getBaublesHandler((EntityPlayer) player);
+        IBaublesItemHandler inv = BaublesApi.getBaublesHandler((EntityPlayer) player);
 
-		int level = -1;
-		for (int slot:BaubleType.RING.getValidSlots()) {
-			ItemStack ring1 = inv.getStackInSlot(slot);
-			Potion potion1 = unequipping && ring == ring1 ? null : getPotion(ring1);
-			if (potion1 == potion) {
-				level++;
-			}
-		}
-		if (level > 1) {
-			level = 1;
-		}
-		PotionEffect currentEffect = player.getActivePotionEffect(potion);
-		int currentLevel = currentEffect != null ? currentEffect.getAmplifier() : -1;
-		assert currentLevel <= level || currentEffect != null;
-		if (currentLevel > level && currentEffect.getDuration() <= EFFECT_DURATION) {
-			player.removePotionEffect(potion);
-		}
-		if(currentLevel != level && level != -1 && !player.world.isRemote) {
-			addPotionEffect(player, level, potion);
-		}
-	}
+        int level = -1;
+        for (int slot : BaubleType.RING.getValidSlots()) {
+            ItemStack ring1 = inv.getStackInSlot(slot);
+            Potion potion1 = unequipping && ring == ring1 ? null : getPotion(ring1);
+            if (potion1 == potion) {
+                level++;
+            }
+        }
+        if (level > 1) {
+            level = 1;
+        }
+        PotionEffect currentEffect = player.getActivePotionEffect(potion);
+        int currentLevel = currentEffect != null ? currentEffect.getAmplifier() : -1;
+        assert currentLevel <= level || currentEffect != null;
+        if (currentLevel > level && currentEffect.getDuration() <= EFFECT_DURATION) {
+            player.removePotionEffect(potion);
+        }
+        if (currentLevel != level && level != -1 && !player.world.isRemote) {
+            addPotionEffect(player, level, potion);
+        }
+    }
 
-	@Override
-	public BaubleType getBaubleType(ItemStack arg0) {
-		return BaubleType.RING;
-	}
+    @Override
+    public BaubleType getBaubleType(ItemStack arg0) {
+        return BaubleType.RING;
+    }
 
 }
--- src/main/java/vazkii/potionfingers/PotionFingers.java
+++ src/main/java/vazkii/potionfingers/PotionFingers.java
@@ -15,43 +15,43 @@ import vazkii.arl.recipe.RecipeHandler;
 @Mod(modid = PotionFingers.MOD_ID, name = PotionFingers.MOD_NAME, version = PotionFingers.VERSION, dependencies = PotionFingers.DEPENDENCIES)
 public class PotionFingers {
 
-	public static final String MOD_ID = "potionfingers";
-	public static final String MOD_NAME = "Potion Fingers";
-	public static final String BUILD = "GRADLE:BUILD";
-	public static final String VERSION = "GRADLE:VERSION-" + BUILD;
-	public static final String DEPENDENCIES = "required-after:autoreglib;required-after:baubles;";
-	
-	public static final Potion[] DEFAULT_EFFECTS = {
-			MobEffects.SPEED, MobEffects.HASTE,
-			MobEffects.RESISTANCE, MobEffects.JUMP_BOOST,	
-			MobEffects.STRENGTH, MobEffects.REGENERATION
-	};
-	
-	public static Item ring;
-	
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		ring = new ItemRing();
-		MinecraftForge.EVENT_BUS.register(ring);
+    public static final String MOD_ID = "potionfingers";
+    public static final String MOD_NAME = "Potion Fingers";
+    public static final String BUILD = "GRADLE:BUILD";
+    public static final String VERSION = "GRADLE:VERSION-" + BUILD;
+    public static final String DEPENDENCIES = "required-after:autoreglib;required-after:baubles;";
 
-		RecipeHandler.addOreDictRecipe(new ItemStack(ring), 
-				"LG ", "G G", " G ",
-				'L', new ItemStack(Blocks.LAPIS_BLOCK),
-				'G', "ingotGold");
-		
-		addRingRecipe(MobEffects.SPEED, Items.SUGAR);
-		addRingRecipe(MobEffects.HASTE, "gemEmerald");
-		addRingRecipe(MobEffects.RESISTANCE, "ingotIron");
-		addRingRecipe(MobEffects.JUMP_BOOST, Items.RABBIT_FOOT);
-		addRingRecipe(MobEffects.STRENGTH, Items.BLAZE_POWDER);
-		addRingRecipe(MobEffects.REGENERATION, Items.GHAST_TEAR);
-	}
-	
-	private static void addRingRecipe(Potion potion, Object item) {
-		RecipeHandler.addOreDictRecipe(ItemRing.getRingForPotion(potion), 
-				" I ", "IRI", " I ",
-				'R', new ItemStack(ring),
-				'I', item);
-	}
-	
+    public static final Potion[] DEFAULT_EFFECTS = {
+            MobEffects.SPEED, MobEffects.HASTE,
+            MobEffects.RESISTANCE, MobEffects.JUMP_BOOST,
+            MobEffects.STRENGTH, MobEffects.REGENERATION
+    };
+
+    public static Item ring;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        ring = new ItemRing();
+        MinecraftForge.EVENT_BUS.register(ring);
+
+        RecipeHandler.addOreDictRecipe(new ItemStack(ring),
+                "LG ", "G G", " G ",
+                'L', new ItemStack(Blocks.LAPIS_BLOCK),
+                'G', "ingotGold");
+
+        addRingRecipe(MobEffects.SPEED, Items.SUGAR);
+        addRingRecipe(MobEffects.HASTE, "gemEmerald");
+        addRingRecipe(MobEffects.RESISTANCE, "ingotIron");
+        addRingRecipe(MobEffects.JUMP_BOOST, Items.RABBIT_FOOT);
+        addRingRecipe(MobEffects.STRENGTH, Items.BLAZE_POWDER);
+        addRingRecipe(MobEffects.REGENERATION, Items.GHAST_TEAR);
+    }
+
+    private static void addRingRecipe(Potion potion, Object item) {
+        RecipeHandler.addOreDictRecipe(ItemRing.getRingForPotion(potion),
+                " I ", "IRI", " I ",
+                'R', new ItemStack(ring),
+                'I', item);
+    }
+
 }
